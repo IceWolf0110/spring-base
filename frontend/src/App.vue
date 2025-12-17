09d6eec74e2c990e11e2f92ac1fcbc9ea0f55cc2
icewolf0110 <icewolf0110@gmail.com>
2025-12-17 16:25:32 +0700

    add front end

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import ThemeSwitcherButton from "@/components/web/button/ThemeSwitcherButton.vue";
+</script>
+
+<template>
+    <ThemeSwitcherButton/>
+</template>
+
+<style scoped></style>
